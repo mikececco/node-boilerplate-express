3ad18fc35766fc29f6bb16b22cf8169df02ea5f3
Mike Cecconello <mikececco2000@gmail.com>
2023-05-04 20:39:33 +0200

    Added GET and POST req and finishe challenge

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config()
 
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+
+
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.get('/now', function(req, res, next) {
   req.time = new Date().toString();
@@ -10,15 +21,32 @@ app.get('/now', function(req, res, next) {
   res.json({time: req.time})
 })
 
-app.use(function(req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next()
-})
-
 app.get('/', (req, res) => {
   absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath)
 })
+
+app.get('/:word/echo', (req, res) => {
+  res.json({echo: req.params.word})
+})
+
+app.route('/name')
+  .get((req, res) => {
+    firstName = req.query.first;
+    lastName = req.query.last;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  })
+  .post((req, res) => {
+    firstName = req.body.first;
+    lastName = req.body.last;
+
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  })
 
 app.get('/json', (req, res) => {
   if (process.env.MESSAGE_STYLE === 'uppercase')
